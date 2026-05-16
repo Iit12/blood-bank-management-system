780706f1f786f570fce0e52adf3605aa5531701f
codebind <abhi.fcb94720@gmail.com>
2026-05-16 15:29:35 +0530

    Moved backend files to root for deployment

--- routes/server.js
+++ routes/server.js
@@ -12,11 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 // Routes with absolute relative paths
-const authRoutes = require('./routes/authRoutes');
-const donorRoutes = require('./routes/donorRoutes');
-const inventoryRoutes = require('./routes/inventoryRoutes');
-const requestRoutes = require('./routes/requestRoutes');
-const issuanceRoutes = require('./routes/issuanceRoutes');
+const authRoutes = require('./authRoutes');
+const donorRoutes = require('./donorRoutes');
+const inventoryRoutes = require('./inventoryRoutes');
+const requestRoutes = require('./requestRoutes');
+const issuanceRoutes = require('./issuanceRoutes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/donors', donorRoutes);
